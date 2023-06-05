2b5eb456715480a79697c6e0b7ebd2986365a7f7
Nicklas Ansman <nicklas@ansman.se>
2023-06-05 09:55:54 -0400

    Fix a bug in the component validation
    If the inferred component and the installIn component were the same,
    then validation would fail.

--- compiler/src/test/resources/tests/auto_bind/component_validation/Repsitory.kt
+++ compiler/src/test/resources/tests/auto_bind/component_validation/Repsitory.kt
@@ -1,14 +1,15 @@
-package tests.auto_bind.child_component
+package tests.auto_bind.component_validation
 
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.components.FragmentComponent
+import dagger.hilt.components.SingletonComponent
 import se.ansman.dagger.auto.AutoBind
 import javax.inject.Inject
 import javax.inject.Singleton
 
 interface Repository
 
-@AutoBind
+@AutoBind(inComponent = SingletonComponent::class)
 @Singleton
 class GlobalRepository @Inject constructor() : Repository
 
