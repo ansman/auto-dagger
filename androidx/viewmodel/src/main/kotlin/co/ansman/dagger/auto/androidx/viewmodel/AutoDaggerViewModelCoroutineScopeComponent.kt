2387bc4d3b7748e18062bc1254181231b79f77ce
Nicklas Ansman <nicklas@ansman.se>
2023-09-06 20:50:35 -0400

    Remove the deprecated typealiases

--- androidx/viewmodel/src/main/kotlin/co/ansman/dagger/auto/androidx/viewmodel/AutoDaggerViewModelCoroutineScopeComponent.kt
+++
@@ -1,8 +0,0 @@
-package co.ansman.dagger.auto.androidx.viewmodel
-
-@Deprecated(
-    message = "This module was incorrectly added to the wrong package. Use se.ansman.dagger.auto.androidx.viewmodel.AutoDaggerViewModelCoroutineScopeComponent instead.",
-    replaceWith = ReplaceWith("AutoDaggerViewModelCoroutineScopeComponent", "se.ansman.dagger.auto.androidx.viewmodel.AutoDaggerViewModelCoroutineScopeComponent"),
-    level = DeprecationLevel.ERROR,
-)
-typealias AutoDaggerViewModelCoroutineScopeComponent = se.ansman.dagger.auto.androidx.viewmodel.AutoDaggerViewModelCoroutineScopeComponent
