cabd3d792c7e09faaa49007b8b339a8bf686a191
Nicklas Ansman <nicklas@ansman.se>
2025-04-26 13:45:51 -0400

    Remove the TODO

--- build-logic/src/main/kotlin/dokka-common.gradle.kts
+++ build-logic/src/main/kotlin/dokka-common.gradle.kts
@@ -1,6 +1,5 @@
 
 import org.gradle.accessors.dm.LibrariesForLibs
-import org.jetbrains.dokka.gradle.AbstractDokkaTask
 
 plugins {
     id("org.jetbrains.dokka")
@@ -11,17 +10,3 @@ val libs = the<LibrariesForLibs>()
 dependencies {
     dokkaPlugin(libs.dokka.versioningPlugin)
 }
-
-// TODO: Check if needed
-//pluginManager.withPlugin("org.jetbrains.kotlin.kapt") {
-//    pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
-//        tasks.withType<AbstractDokkaTask>().configureEach {
-//            dependsOn("kaptKotlin")
-//        }
-//    }
-//    pluginManager.withPlugin("com.android.library") {
-//        tasks.withType<AbstractDokkaTask>().configureEach {
-//            dependsOn("kaptReleaseKotlin", "kaptDebugKotlin")
-//        }
-//    }
-//}
